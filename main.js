ec345d6cf956adb68f2206d7f69378eb945fb9e4
da-dfdf <88136653+da-dfdf@users.noreply.github.com>
2021-08-21 10:39:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var a = Math.floor(Math.random()*5);
+var b = Math.floor(Math.random()*8);
+
+console.log("var a =", "", a );
+console.log("var b =", "", b );
+
+
+var word = "word";
+
+console.log(word, "", "this is a word");
+
+var word2 = word;
+
+console.log(word + word2 + "" + "these are 2 words")
+
+
+var add = a + b;
+console.log(add)
+
